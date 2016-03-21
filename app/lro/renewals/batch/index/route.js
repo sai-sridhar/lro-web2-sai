0c817f1d4697eaf0d721b1a615a568d6acb27b64
Jason Trkovsky <jason.trkovsky@gmail.com>
2016-03-21 10:50:16 -0400

    fix renewal batch routing and model

--- app/lro/renewals/batch/index/route.js
+++ app/lro/renewals/batch/index/route.js
@@ -1,13 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model : function(params) {
-		return this.store.findRecord("renewalBatch", params.batch_id);
-	},
-	afterModel : function(params) {
-		this.store.query('renewalComm', { batch : params.batch_id });
-		this.store.query('renewalUnit', { batch : params.batch_id });
-	},
 	actions : {
 		deleteBatch : function() {
 			var self = this;
--- app/lro/renewals/batch/route.js
+++ app/lro/renewals/batch/route.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model : function(params) {
+		console.log("batch_id: ", params.batch_id);
 		return this.store.findRecord("renewalBatch", params.batch_id);
-	}
+	},
+	afterModel : function(params) {
+		this.store.query('renewalComm', { batch : params.batch_id });
+		this.store.query('renewalUnit', { batch : params.batch_id });
+	},
 });
