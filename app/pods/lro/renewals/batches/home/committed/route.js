5e89c6b16f618a66ebb0c39645cd86aaa96772ca
Jason Trkovsky <jason.trkovsky@gmail.com>
2016-08-10 16:31:09 -0400

    only list batches the user created except for Jason

--- app/pods/lro/renewals/batches/home/committed/route.js
+++ app/pods/lro/renewals/batches/home/committed/route.js
@@ -1,7 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	session : Ember.inject.service('session'),
+
 	model : function() {
-		return this.store.query("renewalBatch", { status : "Committed" });
+		let user = this.get("session.session.content.authenticated.user"),
+			email = user.email,
+			user_id = user._id;
+		if( email === "jtrkovsky@letitrain.com") {
+			return this.store.query("renewalBatch", { status : "Committed" });
+		} else {
+			return this.store.query("renewalBatch", { status : "Committed", createdBy : user_id });
+		}
 	}
 });
